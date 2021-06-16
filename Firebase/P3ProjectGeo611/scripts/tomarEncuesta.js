a9500991fed6f81e4ad336fa001034e2e23ac1ca
VictorElGringo <Victor.education462@gmail.com>
2021-06-16 00:38:02 -0500

    Update tomarEncuesta.js

--- Firebase/P3ProjectGeo611/scripts/tomarEncuesta.js
+++ Firebase/P3ProjectGeo611/scripts/tomarEncuesta.js
@@ -19,8 +19,8 @@ formguisado.addEventListener('submit', (e)=>{
     btnGuisado1.innerHTML = guisado1;
     btnGuisado2.innerHTML = guisado2;
 
-    btnGuisadosRating.forEach(item => item.style.display = 'block');
-    btnGuisadoFinal.forEach(item => item.style.display = 'block');
+    btnGuisadosRating.forEach(item => item.style.display = 'contents');
+    btnGuisadoFinal.forEach(item => item.style.display = 'contents');
     listaloggedinIngresaGuisados.forEach(item => item.style.display = 'none');
 
     $('#guisadoModal').modal('hide');
