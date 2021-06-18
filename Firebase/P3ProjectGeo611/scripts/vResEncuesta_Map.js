7124e734cfe6b6a34bce28341398e48fd8000201
VictorElGringo <Victor.education462@gmail.com>
2021-06-17 21:39:58 -0500

    Update vResEncuesta_Map.js

--- Firebase/P3ProjectGeo611/scripts/vResEncuesta_Map.js
+++ Firebase/P3ProjectGeo611/scripts/vResEncuesta_Map.js
@@ -17,7 +17,7 @@ const obtieneDiasEncuesta = (data) => {
             const encuesta = doc.data();
 
             const columna =`
-                            <div class="card" class="col-4">
+                            <div class="card p-2" class="col-12">
                                 <div class="card-body">
                                     <div class="row">
                                     <div class="col-12">
