44347c44fb7128040a3de2f30a829dd899a09227
VictorElGringo <Victor.education462@gmail.com>
2021-06-17 19:13:12 -0500

    Update viewEncuestaResults.js

--- Firebase/P3ProjectGeo611/scripts/viewEncuestaResults.js
+++ Firebase/P3ProjectGeo611/scripts/viewEncuestaResults.js
@@ -6,15 +6,9 @@ const obtieneDiasEncuesta = (data) => {
 
         data.forEach( doc => {
             const encuesta = doc.data();
-            pos = {
-                lat : encuesta.coordenadas.lat,
-                lng : encuesta.coordenadas.lng
-            };
+
             const columna =`
                             <div class="card" class="col-4">
-                                <div class="text-center" id="map" style="height: fit-content;">
-                                    VA IR AL MAPA
-                                </div>
                                 <div class="card-body">
                                     <div class="row">
                                     <div class="col-12">
@@ -37,25 +31,7 @@ const obtieneDiasEncuesta = (data) => {
                                 </div>
                             </div>
                             `;
-                            var propiedades = { 
-                                center: { 
-                                            lat: 21.152639, lng: -101.711598 
-                                        }, 
-                                zoom: 14 
-                            }
-                         
-                            var mapa =  document.getElementById("map")
-                            var map = new google.maps.Map(mapa, propiedades);
-                         
-                         
-                                
-                                informacion = new google.maps.InfoWindow;
-                         
-                                informacion.setPosition(pos);
-                                informacion.setContent(data.nombre);
-                                informacion.open(map);
-                         
-                
+                                         
 
             html += columna;
 
